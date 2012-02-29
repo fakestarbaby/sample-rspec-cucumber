285f0cfa72706dbf68a0dca30bdbcc8f0fa246dc
fakestarbaby <fakestarbaby@gmail.com>
2012-02-29 15:54:11 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRspecCucumber::Application.config.secret_token = 'e0b2188a20de37ccdd5920f922d57e82a01818e98b165c71b61d5cc55ad2d492d5362a5a76cb961a1dba7a2c1ad0c3fadd5596eee7f42b6fac89ca5fe3ef7adc'
